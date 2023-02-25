cd85de43c3ed5cc4cb74abe6f577c2ce490953fd
Qyabro <ing.andresmejia@gmail.com>
2023-02-25 17:23:16 -0500

    Se implementa CRUD para tabla autores

--- backend/src/app.py
+++ backend/src/app.py
@@ -28,6 +28,87 @@ def versionDb():
         return "Error"
 
 
+#Lista de todos los autores:
+@app.route('/autores')
+def consulta_autores():
+    try:
+        cursor = conexion.connection.cursor()
+        sentencia = "SELECT * FROM lista_autores;"
+        cursor.execute(sentencia)
+        datos=cursor.fetchall()
+        autores=[]
+        for fila in datos:
+            autor={'id':fila[0],'nombre':fila[1]}
+            autores.append(autor)
+        return jsonify({'autores':autores,'mensaje':"ok"})        
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+
+#Informacion de un autor especifico:
+@app.route('/autores/<id>', methods=['GET'])
+def autor_especifico(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sentencia = "SELECT * FROM lista_autores WHERE id_autor={0};".format(id)
+        print(sentencia)
+        cursor.execute(sentencia)
+        datos=cursor.fetchone()
+        if datos != None:   #Si no esta en null el campo
+            infoAutor = {'id':datos[0],'nombre':datos[1]}
+            return jsonify({'infoautor':infoAutor,'mensaje':"ok"}) 
+        else:
+            return jsonify({'mensaje':"Autor no encontrado"})       
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+
+#Crear autor:
+@app.route('/autores', methods=['POST'])
+def registrar_autor():
+    try:
+        #print(request.json)
+        cursor = conexion.connection.cursor()
+        sentencia = "INSERT INTO lista_autores (nombre) VALUES ('{0}');".format(request.json['nombre'])
+        cursor.execute(sentencia)
+        conexion.connection.commit()    #Confirma la accion de insertar dato
+        return jsonify({'mensaje':"ok"})        
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+
+#Editar autor:
+@app.route('/autores/<id>', methods=['PUT'])
+def editar_autor(id):
+    try:
+        #print(request.json)
+        #print(autor)
+        cursor = conexion.connection.cursor()
+        sentencia = """UPDATE lista_autores SET 
+        nombre = '{0}'
+        WHERE 
+        id_autor='{1}';""".format(request.json['nombre'],id)
+        print(sentencia)
+        cursor.execute(sentencia)
+        conexion.connection.commit()    #Confirma la accion de insertar dato
+        return jsonify({'mensaje':"ok"})        
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+
+#Eliminar autor:
+@app.route('/autores/<id>', methods=['DELETE'])
+def eliminar_autor(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sentencia = "DELETE FROM lista_autores WHERE id_autor='{0}'".format(id)
+        cursor.execute(sentencia)
+        conexion.connection.commit()    #Confirma la accion de insertar dato
+        return jsonify({'mensaje':"ok"})        
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+
 #Manejador de errores cuando se intenta ingresar a una pagina que no existe:
 def pagina_no_Encontrada(error):
     return "<h1 style='color: white;background-color:red;'>La pagina que intentas buscar no existe!</h1>", 404 #Se agrega el codigo de error 404
